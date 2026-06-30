ead72256bfa871d32b979a65c09002bf018bf8c7
Sabrina Usvelli <sabrinausvelli@gmail.com>
2026-07-01 00:23:48 +0200

    creazione css, messo loader, inizio nav e footer

---
+++ assets/js/loader.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader")?.remove();
+        fadeIn();
+    }, 770);
+});
+window.addEventListener("scroll", fadeIn);
+function fadeIn() {
+    const elementsArray = document.querySelectorAll(".scroll-fade-in");
+    elementsArray.forEach(elem => {
+        const distInView = elem.getBoundingClientRect().top - window.innerHeight + 40;
+        if (distInView < 0) {
+            elem.classList.add("visible");
+        } else {
+            elem.classList.remove("visible");
+        }
+    });
+}
